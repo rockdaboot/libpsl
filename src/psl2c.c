8b096a5edadc07715ed99fdedecdf72647cec215
Tim Ruehsen <tim.ruehsen@gmx.de>
2014-03-24 20:41:46 +0100

    LGPL->MIT license, some cleanups

--- src/psl2c.c
+++ src/psl2c.c
@@ -1,21 +1,25 @@
 /*
  * Copyright(c) 2014 Tim Ruehsen
  *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
  * This file is part of libpsl.
- *
- * Libpsl is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Libpsl is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with libpsl.  If not, see <http://www.gnu.org/licenses/>.
- *
  *
  * Precompile Public Suffix List into
  *
@@ -107,25 +111,6 @@ static _psl_entry_t *_vector_get(const _psl_vector_t *v, int pos)
 	if (pos < 0 || !v || pos >= v->cur) return NULL;
 
 	return v->entry[pos];
-}
-
-// the entries must be sorted by
-static int _vector_find(const _psl_vector_t *v, const _psl_entry_t *elem)
-{
-	if (v) {
-		int l, r, m;
-		int res;
-
-		// binary search for element (exact match)
-		for (l = 0, r = v->cur - 1; l <= r;) {
-			m = (l + r) / 2;
-			if ((res = v->cmp(elem, v->entry[m])) > 0) l = m + 1;
-			else if (res < 0) r = m - 1;
-			else return m;
-		}
-	}
-
-	return -1; // not found
 }
 
 static int _vector_add(_psl_vector_t *v, const _psl_entry_t *elem)
--- tests/test-is-public.c
+++ tests/test-is-public.c
@@ -1,23 +1,27 @@
 /*
  * Copyright(c) 2014 Tim Ruehsen
  *
- * This file is part of MGet.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
  *
- * Mget is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * Mget is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  *
- * You should have received a copy of the GNU General Public License
- * along with Mget.  If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of the test suite of libpsl.
  *
- *
- * Public Suffix List routines (right now experimental)
+ * Test case for psl_load_file(), psl_is_public(), psl_free()
  *
  * Changelog
  * 19.03.2014  Tim Ruehsen  created from libmget/cookie.c
