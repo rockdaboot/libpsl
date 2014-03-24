eaf4f01b78ebff8eabca3a12219f4fe1c2b9c7ed
Tim Ruehsen <tim.ruehsen@gmx.de>
2014-03-24 15:53:33 +0100

    added check for idn2

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,12 @@
 # !/bin/sh -e
 
 if test -z `which autoreconf`; then
-  echo "No autoreconf found. You must install the autoconf package."
+  echo "No 'autoreconf' found. You must install the autoconf package."
+  exit 1
+fi
+
+if test -z `which idn2`; then
+  echo "No 'idn2' found. You must install the idn2 package."
   exit 1
 fi
 
