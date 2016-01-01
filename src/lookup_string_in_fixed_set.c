23345f5f37684832dd672a7a247c653e83141a53
Tim Rühsen <tim.ruehsen@gmx.de>
2016-01-01 22:31:01 +0100

    Convert lookup_string_in_fixed_set.c into UTF-8

--- src/lookup_string_in_fixed_set.c
+++ src/lookup_string_in_fixed_set.c
@@ -2,7 +2,7 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE.chromium file.
  *
- * Converted to C89 2015 by Tim Rühsen
+ * Converted to C89 2015 by Tim RÃ¼hsen
  */
 
 #include <stddef.h>
