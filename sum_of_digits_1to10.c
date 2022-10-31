281af2ae7dbeab45a178fcaffb9d7f838675cd30
Ruteswar <114854038+Ruteswar@users.noreply.github.com>
2022-10-31 20:35:30 +0530

    Add files via upload

---
+++ sum_of_digits_1to10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int sum()
+{
+    int a=0;
+    int num;
+    for (num=0;num>=10;num++)
+    {
+        a+=num;
+    }
+    return a;
+}
+void main()
+{
+    if (a=sum);
+    printf("sum of digits from 1 to 10 %d");
+    getch();
+}
