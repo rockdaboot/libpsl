595d259f68f11d360f64d14191c1b4d3132d56ab
Tim Rühsen <tim.ruehsen@gmx.de>
2024-02-04 18:01:20 +0100

    Drop usage of alloca() in libpsl
    Reported-by: @rsbeckerca (github.com)

--- src/psl.c
+++ src/psl.c
@@ -69,12 +69,8 @@ typedef SSIZE_T ssize_t;
 
 #ifndef _WIN32
 # include <arpa/inet.h>
-#endif
-
-#ifdef HAVE_ALLOCA_H
-#	include <alloca.h>
-#elif defined _WIN32
-#	include <malloc.h>
+#else
+# include <malloc.h>
 #endif
 
 #ifdef WITH_LIBICU
@@ -1753,23 +1749,31 @@ psl_error_t psl_str_to_utf8lower(const char *str, const char *encoding, const ch
 	}
 
 #ifdef WITH_LIBICU
+#define STACK_STRLENGTH 256
 	do {
-	size_t str_length = strlen(str);
 	UErrorCode status = 0;
 	UChar *utf16_dst, *utf16_lower;
-	int32_t utf16_dst_length;
 	char *utf8_lower;
+	int32_t utf16_dst_length, utf16_dst_size, utf16_lower_size, utf8_lower_size;
 	UConverter *uconv;
+	UChar utf16_dst_buf[STACK_STRLENGTH * 2 + 1];
+	UChar utf16_lower_buf[STACK_STRLENGTH * 2 + 1];
+	char utf8_lower_buf[STACK_STRLENGTH * 6 + 1];
+	size_t str_length = strlen(str);
 
-	if (str_length < 256) {
-		/* C89 allocation */
-		utf16_dst   = alloca(sizeof(UChar) * (str_length * 2 + 1));
-		utf16_lower = alloca(sizeof(UChar) * (str_length * 2 + 1));
-		utf8_lower  = alloca(str_length * 6 + 1);
+	if (str_length <= STACK_STRLENGTH) {
+		utf16_dst_size = countof(utf16_dst_buf);
+		utf16_lower_size = countof(utf16_lower_buf);
+		utf8_lower_size = countof(utf8_lower_buf);
+		utf16_dst   = utf16_dst_buf;
+		utf16_lower = utf16_lower_buf;
+		utf8_lower  = utf8_lower_buf;
 	} else {
-		utf16_dst   = malloc(sizeof(UChar) * (str_length * 2 + 1));
-		utf16_lower = malloc(sizeof(UChar) * (str_length * 2 + 1));
-		utf8_lower  = malloc(str_length * 6 + 1);
+		utf16_dst_size = utf16_lower_size = str_length * 2 + 1;
+		utf8_lower_size = str_length * 6 + 1;
+		utf16_dst   = malloc(sizeof(UChar) * utf16_dst_size);
+		utf16_lower = malloc(sizeof(UChar) * utf16_lower_size);
+		utf8_lower  = malloc(sizeof(char) * utf8_lower_size);
 
 		if (!utf16_dst || !utf16_lower || !utf8_lower) {
 			ret = PSL_ERR_NO_MEM;
@@ -1779,13 +1783,13 @@ psl_error_t psl_str_to_utf8lower(const char *str, const char *encoding, const ch
 
 	uconv = ucnv_open(encoding, &status);
 	if (U_SUCCESS(status)) {
-		utf16_dst_length = ucnv_toUChars(uconv, utf16_dst, str_length * 2 + 1, str, str_length, &status);
+		utf16_dst_length = ucnv_toUChars(uconv, utf16_dst, utf16_dst_size, str, str_length, &status);
 		ucnv_close(uconv);
 
 		if (U_SUCCESS(status)) {
-			int32_t utf16_lower_length = u_strToLower(utf16_lower, str_length * 2 + 1, utf16_dst, utf16_dst_length, locale, &status);
+			int32_t utf16_lower_length = u_strToLower(utf16_lower, utf16_lower_size, utf16_dst, utf16_dst_length, locale, &status);
 			if (U_SUCCESS(status)) {
-				u_strToUTF8(utf8_lower, str_length * 6 + 1, NULL, utf16_lower, utf16_lower_length, &status);
+				u_strToUTF8(utf8_lower, utf8_lower_size, NULL, utf16_lower, utf16_lower_length, &status);
 				if (U_SUCCESS(status)) {
 					ret = PSL_SUCCESS;
 					if (lower) {
@@ -1813,11 +1817,13 @@ psl_error_t psl_str_to_utf8lower(const char *str, const char *encoding, const ch
 		/* fprintf(stderr, "Failed to open converter for '%s' (status %d)\n", encoding, status); */
 	}
 out:
-	if (str_length >= 256) {
+	if (utf16_dst != utf16_dst_buf)
 		free(utf16_dst);
+	if (utf16_lower != utf16_lower_buf)
 		free(utf16_lower);
+	if (utf8_lower != utf8_lower_buf)
 		free(utf8_lower);
-	}
+
 	} while (0);
 #elif defined(WITH_LIBIDN2) || defined(WITH_LIBIDN)
 	do {
