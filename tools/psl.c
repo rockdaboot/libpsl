d5a5c094f0dff2f27928483a9b927e02f8d8612f
Tim Rühsen <tim.ruehsen@gmx.de>
2026-07-05 22:53:35 +0200

    Simplify license headers

--- tools/psl.c
+++ tools/psl.c
@@ -1,30 +1,11 @@
 /*
- * Copyright(c) 2014-2024 Tim Ruehsen
+ * SPDX-License-Identifier: MIT
  *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
+ * See the LICENSE file in the root directory for details and copyrights.
  *
  * This file is part of libpsl.
  *
  * Using the libpsl functions via command line
- *
- * Changelog
- * 11.04.2014  Tim Ruehsen  created
  *
  */
 
@@ -161,7 +142,7 @@ int main(int argc, const char *const *argv)
 				printf("psl %s (0x%06x)\n", PACKAGE_VERSION, psl_check_version_number(0));
 				printf("libpsl %s\n", psl_get_version());
 				printf("\n");
-				printf("Copyright (C) 2014-2024 Tim Ruehsen\n");
+				printf("Copyright (C) Tim Ruehsen and Libpsl Contributors\n");
 				printf("License: MIT\n");
 				exit(0);
 			}
